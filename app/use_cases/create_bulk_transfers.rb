f730ea9eb930177a7f9c08e29d68e2bb69bdeda4
Rafael Guimarães <rafael.guimaraes@sequra.es>
2023-11-11 12:27:25 +0100

    InsufficientBalance validation when creating bulk transfers

--- app/use_cases/create_bulk_transfers.rb
+++ app/use_cases/create_bulk_transfers.rb
@@ -1,22 +1,30 @@
 class CreateBulkTransfers
-  def initialize(overrides = {})
+  def initialize(create_transfer_command, overrides = {})
+    @create_transfer_command = create_transfer_command
     @bank_account_repository = overrides.fetch(:bank_account_repository) do
       BankAccount
     end
   end
 
-  def create(create_transfer_command)
-    @bank_account_repository.find_bank_account_by(
-      organization_name: create_transfer_command.organization_name,
-      bic: create_transfer_command.bic,
-      iban: create_transfer_command.iban
-    ).tap do |account|
-      raise BankAccountNotFoundError unless account
-    end
+  def create
+    raise InsufficientBalanceError unless bank_account.sufficient_balance_for_transaction?(transaction_total_amount_cents)
   end
 
   private
 
   def bank_account
+    @bank_account ||= @bank_account_repository.find_bank_account_by(
+      organization_name: @create_transfer_command.organization_name,
+      bic: @create_transfer_command.bic,
+      iban: @create_transfer_command.iban
+    ).tap do |account|
+      raise BankAccountNotFoundError unless account
+    end
+  end
+
+  def transaction_total_amount_cents
+    @create_transfer_command.credit_transfers.map do |credit_transfer|
+      (BigDecimal(credit_transfer.amount.to_s) * 100).to_i
+    end.sum
   end
 end
--- spec/use_cases/create_bulk_transfers_spec.rb
+++ spec/use_cases/create_bulk_transfers_spec.rb
@@ -11,20 +11,20 @@ RSpec.describe CreateBulkTransfers do
           bic: "OIVUSCLQXXX",
           iban: "FR10474608000002006107XXXXX",
           credit_transfers: [
-            {
+            OpenStruct.new(
               amount: "14.5",
               counterparty_name: "Bip Bip",
               counterparty_bic: "CRLYFRPPTOU",
               counterparty_iban: "EE383680981021245685",
               description: "Wonderland/4410"
-            },
-            {
+            ),
+            OpenStruct.new(
               amount: "61238",
               counterparty_name: "Wile E Coyote",
               counterparty_bic: "ZDRPLBQI",
               counterparty_iban: "DE9935420810036209081725212",
               description: "//TeslaMotors/Invoice/12"
-            }
+            )
           ]
         )
         allow(bank_account_repository).to receive(:find_bank_account_by)
@@ -36,14 +36,15 @@ RSpec.describe CreateBulkTransfers do
 
         expect {
           described_class.new(
+            create_transfer_command,
             bank_account_repository:
-          ).create(create_transfer_command)
+          ).create
         }.to raise_error(BankAccountNotFoundError)
       end
     end
 
     context "when there isn't enough balance to proccess the transfers" do
-      xit "raises InsufficientBalanceError" do
+      it "raises InsufficientBalanceError" do
         bank_account_repository = class_double(BankAccount)
         bank_account = instance_double(BankAccount)
         create_transfer_command = double(
@@ -52,20 +53,20 @@ RSpec.describe CreateBulkTransfers do
           bic: "OIVUSCLQXXX",
           iban: "FR10474608000002006107XXXXX",
           credit_transfers: [
-            {
+            OpenStruct.new(
               amount: "14.5",
               counterparty_name: "Bip Bip",
               counterparty_bic: "CRLYFRPPTOU",
               counterparty_iban: "EE383680981021245685",
               description: "Wonderland/4410"
-            },
-            {
+            ),
+            OpenStruct.new(
               amount: "61238",
               counterparty_name: "Wile E Coyote",
               counterparty_bic: "ZDRPLBQI",
               counterparty_iban: "DE9935420810036209081725212",
               description: "//TeslaMotors/Invoice/12"
-            }
+            )
           ]
         )
         allow(bank_account_repository).to receive(:find_bank_account_by)
@@ -74,13 +75,14 @@ RSpec.describe CreateBulkTransfers do
             bic: create_transfer_command.bic,
             iban: create_transfer_command.iban,
           ).and_return(bank_account)
-        allow(bank_account).to receive(:sufficient_balance_for?)
-          .with("6125250.0").and_return(false)
+        allow(bank_account).to receive(:sufficient_balance_for_transaction?)
+          .with(6125250).and_return(false)
 
         expect {
           described_class.new(
+            create_transfer_command,
             bank_account_repository:
-          ).create(create_transfer_command)
+          ).create
         }.to raise_error(InsufficientBalanceError)
       end
     end
