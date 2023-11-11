37c7ea57f621df440f06dc10ec51884c9479bb99
Rafael Guimarães <rafael.guimaraes@sequra.es>
2023-11-11 12:11:04 +0100

    Checking bank account sufficient balance for transaction

--- app/models/bank_account.rb
+++ app/models/bank_account.rb
@@ -4,4 +4,8 @@ class BankAccount < ApplicationRecord
       organization_name:, bic:, iban:
     ).first
   end
+
+  def sufficient_balance_for_transaction?(amount_in_cents)
+    balance_cents >= amount_in_cents
+  end
 end
